4688f6fe2919214f4fc8b663fe9e7d64f6eb0343
Quin-shi-huan <phelipee06@gmail.com>
2025-04-02 09:24:09 -0300

    fix: fixed the status_code of http_unprocessable_entity.py

--- src/errors/error_type/http_unprocessable_entity.py
+++ src/errors/error_type/http_unprocessable_entity.py
@@ -1,6 +1,6 @@
 class HtppUnprocessableEntityError(Exception):
     def __init__(self, message: str) -> None:
         super().__init__(message)
-        self.status_code = 402
+        self.status_code = 422
         self.name = "UnprocessableEntity"
         self.message = message
